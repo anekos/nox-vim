4085f79ca9bd6402679c5b92109bda74a8d20530
anekos <anekos@snca.net>
2023-11-17 12:25:05 +0900

    Use lua API more

--- lua/nox/api.lua
+++ lua/nox/api.lua
@@ -55,6 +55,10 @@ end
 
 -- }}}
 
+function M.new_document_from_source(id, content)
+  return request('post', '/api/source', vanish { id = id }, content)
+end
+
 function M.update_document_from_source(id, content, update_from)
   return request('put', '/api/source', vanish { id = id, update_from = update_from }, content)
 end
