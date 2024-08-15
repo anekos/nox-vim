66213597d1739ec939742baf0b00422ff749c202
anekos <anekos@snca.net>
2024-08-15 15:24:56 +0900

    Fix `NoxTasks`

--- lua/telescope/_extensions/nox/previewer.lua
+++ lua/telescope/_extensions/nox/previewer.lua
@@ -2,21 +2,16 @@ local previewers = require('telescope.previewers')
 
 return previewers.new_buffer_previewer {
   title = 'Config',
-  define_preview = function (self, entry, _)
+  define_preview = function(self, entry, _)
     local id = entry.value
+    id = id:gsub('#.*', '')
 
     local lines = vim.fn['nox#api#get_source'](id)
 
-    if #lines == 0 then
-      table.insert(lines, 'Empty')
+    if lines == vim.NIL then
+      lines = { 'Empty' }
     end
 
-    vim.api.nvim_buf_set_lines(
-      self.state.bufnr,
-      0,
-      -1,
-      false,
-      lines
-    )
-  end
+    vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+  end,
 }
