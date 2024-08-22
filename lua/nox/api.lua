36e9c53e3313e6bbd5cacf28a09b48661e114f2d
anekos <anekos@snca.net>
2024-08-22 18:21:37 +0900

    Sort it

--- lua/nox/api.lua
+++ lua/nox/api.lua
@@ -65,14 +65,14 @@ function M.new_document_from_source(id, source, meta)
   return request('post', '/api/source', vanish(params), body)
 end
 
+function M.resolve_ulid(ulid)
+  return request('get', '/api/ulid', vanish { ulid = ulid })
+end
+
 function M.update_document_from_source(id, source, meta, update_from)
   local params = { id = id, update_from = update_from, sync = '1' }
   local body = { source = source, meta = meta }
   return request('put', '/api/source', vanish(params), body)
-end
-
-function M.resolve_ulid(ulid)
-  return request('get', '/api/ulid', vanish { ulid = ulid })
 end
 
 return M
